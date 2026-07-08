fe9e9107d3110c0160be3ed74073c1abb4f3aff0
Cambelliino .. <cambelliino..@Cambelliinos-MacBook-Pro.local>
2026-07-08 14:16:41 +0100

    chore: align art exhibition event details with calendar definitions

--- src/app/artexhibition/page.tsx
+++ src/app/artexhibition/page.tsx
@@ -6,8 +6,8 @@ export default function Page() {
         <EventRSVPForm
             eventTitle="VVS Lagos 2026 Art Exhibition"
             eventDate="July 9 - 12, 2026"
-            eventTime="12:00 PM WAT"
-            eventVenue="Blank Space, VI, Lagos"
+            eventTime="11:00 AM WAT"
+            eventVenue="Blank Space, Grace Arena Plaza, VI, Lagos"
             eventDescription="VVS Lagos 2026 Art Exhibition: showcasing outstanding modern and contemporary visual art installations."
             eventType="art_exhibition"
             preSelectedEvents={["JULY 9-12"]}
